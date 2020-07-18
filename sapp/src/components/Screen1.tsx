53c8dabff4e401522cdd2046e7bb7b3b34051f60
lukasgnosis <lukas.schor@gnosis.pm>
2020-07-18 16:31:13 +0200

    ui for first prototype

--- sapp/src/components/Screen1.tsx
+++ sapp/src/components/Screen1.tsx
@@ -2,27 +2,74 @@
 import React, { useState, useCallback, useMemo, useEffect } from 'react'
 import { Button, Card, Icon, Title, Text } from '@gnosis.pm/safe-react-components';
 import TextField from '@material-ui/core/TextField';
+import styled from 'styled-components';
+import onboarding1 from './image1.svg';
+import onboarding2 from './image2.svg';
+import onboarding3 from './image3.svg';
 import { useSafe } from '@rmeissner/safe-apps-react-sdk';
 import '../App.css';
+
 
 interface Props {
   action: () => void
 }
-const Screen1: React.FC<Props> = ({action}) => {
+
+const TextContainer = styled.div`
+display: flex;
+justify-content: space-evenly;
+text-align: center;
+margin: 3rem 0 0rem 0;
+`;
+
+const Screen1: React.FC<Props> = ({ action }) => {
   const safe = useSafe()
   return (
     <>
       <Text size="xl" strong>Safe24 - Your gas fee subscription service</Text>
       <br /><br />
-      <Text size="md">Safe24 allows you to never mind about transaction fees again. We take care of setting the right gas price, paying the fees and making sure the transaction gets executed on time.</Text>
+      <Text size="lg">Safe24 allows you to never mind about transaction fees again. We take care of setting the right gas price, paying the fees and making sure the transaction gets executed on time.</Text>
       <br /><br />
-      <Text size="md"><b>Important:</b> Safe24 never has access to any of your funds or can initiate a transaction that was not previously confirmed by you.</Text>
+      <Text size="lg"><b>Important:</b> Safe24 never has access to any of your funds or can initiate a transaction that was not previously confirmed by you. Safe24 just relays transactions that have already been signed for this Safe but not yet executed. </Text>
       <br /><br />
       <div>
-        <Button size="md" color="primary" variant="contained" onClick={action}>
+        <Button size="lg" color="primary" variant="contained" onClick={action}>
           become member
     </Button>
       </div>
+
+
+      <TextContainer>
+        <div justify-content="center" style={{ margin: "8px", flex: "1 1 0px"}}>
+        <img src={onboarding1} alt="image"></img>
+          <Text size="xl" strong center>
+            1) Setup Safe
+          </Text>
+          <Text size="lg" center>
+            Become a member of Safe24 for just 5 DAI per week
+          </Text>
+        </div>
+
+        <div justify-content="center" style={{ margin: "8px", flex: "1 1 0px" }}>
+        <img src={onboarding2} alt="image"></img>
+          <Text size="xl" strong center>
+            2) Top up weekly or monthly
+          </Text>
+          <Text size="lg" center>
+            You can extend your membership on a weekly or monthly basis
+          </Text>
+        </div>
+
+        <div justify-content="center" style={{ margin: "8px", flex: "1 1 0px" }}>
+        <img src={onboarding3} alt="image"></img>
+          <Text size="xl" strong center>
+            3) Enjoy free transactions
+  </Text>
+          <Text size="lg" center>
+            We will cover all your transaction fees.
+  </Text>
+        </div>
+      </TextContainer>
+
     </>
   )
 }
--- sapp/src/components/Screen2.tsx
+++ sapp/src/components/Screen2.tsx
@@ -2,7 +2,9 @@
 import React, { useState, useCallback, useMemo, useEffect } from 'react'
 import { Button, Card, Icon, Title, Select, Text } from '@gnosis.pm/safe-react-components';
 import TextField from '@material-ui/core/TextField';
+import styled from 'styled-components';
 import { useSafe } from '@rmeissner/safe-apps-react-sdk';
+import time from './image4.svg';
 import '../App.css';
 
 type SelectItem = {
@@ -16,40 +18,60 @@ interface Props {
 const Screen2: React.FC<Props> = ({ action }) => {
   const safe = useSafe();
   const items: Array<SelectItem> = [
-    { id: '1', label: '1 week' },
-    { id: '2', label: '1 month' },
-    { id: '3', label: '1 year' },
+    { id: '1', label: '1 week ($5)' },
+    { id: '2', label: '1 month ($20)' },
   ];
+
+  const TextContainer = styled.div`
+  display: flex;
+  justify-content: start;
+  margin: 0rem 0 0rem 0;
+  align-items: center
+`;
+
   const [activeItemId, setActiveItemId] = useState('');
   return (
     <>
       <Text size="xl" strong>Safe24 Membership</Text>
       <br /><br />
-      <Text size="md">Your Safe24 membership is valid for:</Text>
+      <Text size="lg">Your membership is valid for:</Text>
+      <br /><br />
+      <TextContainer>
+        <div justify-content="left" style={{ margin: "8px" }}>
+          <img src={time} alt="image"></img>
+        </div>
+        <div justify-content="left" style={{ margin: "8px" }}>
+          <Text size="lg" strong>No membership activated for this Safe</Text>
+        </div>
+      </TextContainer>
       <br /><br />
       <Text size="xl" strong>Top up</Text>
       <br /><br />
-      <Text size="md">Add more funds to extend your Safe24 membership.</Text>
-      <br /><br />
-
-<div>
-      <Select
-        items={items}
-        activeItemId={activeItemId}
-        onItemClick={(id) => {
-          setActiveItemId(id);
-        }}
-      />
-      <Text size="md">2 Dai</Text>
-</div>
+      <Text size="lg">Extend your Safe24 membership for an additional week ($5) or month ($20).</Text>
       <br /><br />
 
 
-      <div>
-        <Button size="md" color="primary" variant="contained" onClick={action}>
-          Pay
+      <TextContainer>
+        <div>
+          <Select
+            items={items}
+            activeItemId={activeItemId}
+            onItemClick={(id) => {
+              setActiveItemId(id);
+            }}
+          />
+        </div>
+        <div style={{ margin: "8px", flex: "1 1 0px"}}>
+          <Button size="lg" color="primary" variant="contained" onClick={action}>
+            Extend membership
     </Button>
-      </div>
+        </div>
+      </TextContainer>
+
+
+
+
+
     </>
   )
 }
